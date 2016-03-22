ec4011ea521ff02f4ad3f710d1244facce9c3bef
Chris O'Connell <chris@Chriss-MacBook-Pro.local>
2016-03-21 17:14:02 -0700

    inital commit

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp'),
+  nodemon = require('gulp-nodemon'),
+  plumber = require('gulp-plumber'),
+  livereload = require('gulp-livereload'),
+  bourbon = require("node-bourbon").includePaths;
+  neat = require("node-neat").includePaths;
+  sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  gulp.src('./public/css/*.scss')
+    .pipe(plumber())
+    .pipe(sass({
+      includePaths: bourbon,
+      includePaths: neat
+      }))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./public/css/*.scss', ['sass']);
+});
+
+gulp.task('develop', function () {
+  livereload.listen();
+  nodemon({
+    script: 'app.js',
+    ext: 'js coffee nunjucks',
+    stdout: false
+  }).on('readable', function () {
+    this.stdout.on('data', function (chunk) {
+      if(/^Express server listening on port/.test(chunk)){
+        livereload.changed(__dirname);
+      }
+    });
+    this.stdout.pipe(process.stdout);
+    this.stderr.pipe(process.stderr);
+  });
+});
+
+gulp.task('default', [
+  'sass',
+  'develop',
+  'watch'
+]);
