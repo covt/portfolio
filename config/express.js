245b62201af19538ecb2dd932f1e57751ffa3450
Chris O'Connell <chris@dsgnby.co>
2016-05-18 00:43:49 -0700

    style changes. footer update.

--- config/express.js
+++ config/express.js
@@ -13,7 +13,7 @@ module.exports = function(app, config) {
   var env = process.env.NODE_ENV || 'development';
   app.locals.ENV = env;
   app.locals.ENV_DEVELOPMENT = env == 'development';
-  
+
   app.set('views', config.root + '/app/views');
   app.set('view engine', 'nunjucks');
   nunjucks.configure(config.root + '/app/views', {
@@ -42,7 +42,7 @@ module.exports = function(app, config) {
     err.status = 404;
     next(err);
   });
-  
+
   if(app.get('env') === 'development'){
     app.use(function (err, req, res, next) {
       res.status(err.status || 500);
